63c4ab142481e401c6436e69e3418552b94fbc9f
RealSebasTheFish <37641875+RealSebasTheFish@users.noreply.github.com>
2021-12-02 09:47:18 -0500

    Updated data structure

--- js/data.js
+++ js/data.js
@@ -3,9 +3,13 @@ playerData = [
         id: "73465834975",
         realname: "Jim",
         email: "",
-        alias: "",
-        discordid: "",
-        points: "",
-        matches: [216321736, 2189371283, 2198391273, 28371938237]
+        // Using custom data so content of the json object can be changed dynamically if needed
+        // If we need to add a new stat or smth, we can update everyone's custom data object and add it
+        customData: {
+            alias: "",
+            discordid: "",
+            points: [],
+            matches: [216321736, 2189371283, 2198391273, 28371938237]
+        }
     }
 ]
