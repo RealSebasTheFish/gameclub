ff1c9da6ae70d9fe3ab10bfab52c3fce8b34b119
nateCode1 <52888031+nateCode1@users.noreply.github.com>
2021-12-02 09:59:51 -0500

    gameProf make

---
+++ js/gameProfile.js
@@ -0,0 +1,16 @@
+games = [
+    {
+        gameId: "87465416541261",
+        gameName: "Smash Bros",
+		pointsPerPlay: 3;
+		customData {
+			metrics: ["kills", "deaths"],
+			kills: {
+				points: 5
+			}
+			deaths:{
+				points: -1
+			}
+		}
+    }
+]
