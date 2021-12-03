a2c296b9aa1d296129793e49bb50c949e9bf82ea
RealSebasTheFish <37641875+RealSebasTheFish@users.noreply.github.com>
2021-12-03 09:13:56 -0500

    Updated file structure a bit more in the js folder

--- website/js/data/gameProfile.js
+++ website/js/data/gameProfile.js
@@ -2,12 +2,12 @@ games = [
     {
         gameId: "87465416541261",
         gameName: "Smash Bros",
-		pointsPerPlay: 3;
-		customData {
+		pointsPerPlay: 3,
+		customData: {
 			metrics: ["kills", "deaths"],
 			kills: {
 				points: 5
-			}
+			},
 			deaths:{
 				points: -1
 			}
